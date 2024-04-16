c9a781e85297aca48c4a0563c6bc1e4021a823c4
Veinoss <imightbot@gmail.com>
2024-04-16 19:50:37 -0400

    Debut de la logique un peu plus clair de la lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,29 +2,34 @@
 pragma solidity 0.8.19;
 
 contract Lottery {
- 
-    uint private storedData;
+     
     address[] public participantList;
+    uint256 private pricePool;
+    uint256 private jackPot;
 
     address immutable player1;
 
     constructor(){
         player1 = 0xbE934830a53E59159de1ff923e927308D4FDc650;
     }
+    
+    function getPricePool() public view returns (uint) {
+        return pricePool;
+    }
 
-    function set(uint  data) public {
-        storedData = data;
-    }
-    function get() public view returns (uint) {
-        return storedData;
-    }
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
+
     function enroleInLottery() external payable {
         require(msg.value == 1 ether);
-        participantList.push(msg.sender);
+        
+        // On garde 10% pour un jackpot potentiel p-e? 1 chance sur 1000 de frappé le bonus
+        pricePool += (msg.value * 90) / 100; 
+        jackPot += (msg.value * 9) / 100;
+        participantList.push(msg.sender);        
     }
+
     function getParticipants() public view returns(address[] memory){
         return participantList;
     }
