b553450ef6ee4c0081f5fb325a5764458ed9599c
Veinoss <imightbot@gmail.com>
2024-04-17 22:34:58 -0400

    Ajout de fonction getWinner et sendPrize, mais zéro tester

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,35 +2,55 @@
 pragma solidity 0.8.19;
 
 contract Lottery {
-     
-    address[] public participantList;
-    uint256 private pricePool;
+    
+    uint256 private prizePool;
     uint256 private jackPot;
 
-    address immutable player1;
+    mapping(address => uint) player;
+    address[] public players;
 
-    constructor(){
-        player1 = 0xbE934830a53E59159de1ff923e927308D4FDc650;
+    constructor(){   
     }
     
     function getPricePool() public view returns (uint) {
-        return pricePool;
+        return prizePool;
     }
 
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
 
-    function enroleInLottery() external payable {
-        require(msg.value == 1 ether);
+    function getJackPot() public view returns (uint) {
+        return jackPot;
+    }
+
+    function enroleInLottery() public payable {
+       require(msg.value == 1 ether || msg.value == 2);
         
+        player[msg.sender] = msg.value;
+        players.push(msg.sender);
+
         // On garde 10% pour un jackpot potentiel p-e? 1 chance sur 1000 de frappé le bonus
-        pricePool += (msg.value * 90) / 100; 
-        jackPot += (msg.value * 9) / 100;
-        participantList.push(msg.sender);        
+        prizePool += (msg.value * 90) / 100; 
+        jackPot += (msg.value * 9) / 100;             
     }
 
     function getParticipants() public view returns(address[] memory){
-        return participantList;
+        return players;
+    }
+
+    function getAWinner() public returns(address){
+        uint nbOfPlayers = players.length;
+        uint randomHash = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
+        uint winnerIndex = randomHash % nbOfPlayers;
+
+        address winner = players[winnerIndex];
+        return winner;
+    }
+
+    function sendPrize(address winner) private {
+        uint prize = prizePool;
+        prizePool = 0;
+        payable(winner).transfer(prize);
     }
 }
--- test/lottery.js
+++ test/lottery.js
@@ -10,4 +10,42 @@ contract("Lottery", function ( accounts ) {
     await Lottery.deployed();
     return assert.isTrue(true);
   });
+ 
+  it("should allow multiple accounts to enroll", async () => {
+    const lottery = await Lottery.deployed();
+    const wager = web3.utils.toWei('1', 'ether');
+    const expectedPricePool = web3.utils.toWei('9', 'ether');
+    const expectedJackPot = web3.utils.toWei('0.9', 'ether');
+
+    for(let i = 0; i < 10; i++) {
+      await lottery.enroleInLottery({from: accounts[i], value: wager});  
+    }  
+
+  const playersCount = await lottery.getParticipants();
+  const pricePool = await lottery.getPricePool();
+  const jackPot = await lottery.getJackPot();
+
+  assert.equal(playersCount.length, 10, "There should be exactly 10 players");
+  assert.equal(expectedPricePool, pricePool, "The pricePool should be 9");
+  assert.equal(expectedJackPot, jackPot, "The jackpot should be 0.9");
+   
+  });
+
+  /*it("should allow multiple accounts to enroll", async () => {
+    const lottery = await Lottery.deployed();
+    const wager = web3.utils.toWei('1', 'ether');
+    const expectedPricePool = web3.utils.toWei('9', 'ether');
+
+    for(let i = 0; i < 10; i++) {
+      await lottery.enroleInLottery({from: accounts[i], value: wager});  
+    }
+  
+
+  const playersCount = await lottery.getParticipants();
+  const pricePool = await lottery.getPricePool();
+  assert.equal(playersCount.length, 10, "There should be exactly 10 players");
+  assert.equal(expectedPricePool, pricePool);
+   
+  });*/
+//})
 });
