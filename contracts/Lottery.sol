31ced88f52fa1710fc49b3c0eee8b08d0461f178
Veinoss <imightbot@gmail.com>
2024-04-19 16:40:12 -0400

    Ajout test pour valider 10 joueur participte, le Jackpot, et getAWinner

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+
+
 contract Lottery {
     address public owner;
     uint256 private jackPotOdds = 10;
@@ -12,6 +14,9 @@ contract Lottery {
 
     event ANewPlayerEnrolled(string message);
 
+    // temporaire
+        event fml(string, address);
+
     constructor(){   
         owner = msg.sender;
     }
@@ -21,9 +26,11 @@ contract Lottery {
         _;
     }
     
-    function setJackPotOdds(uint256 newOdds) public onlyOwner {        
+    function setJackPotOdds(uint256 newOdds) public onlyOwner {   
+        require(newOdds >= 0 && newOdds <= 100);     
         jackPotOdds = newOdds;
     }
+
     function getPricePool() public view returns (uint) {
         return prizePool;
     }
@@ -39,6 +46,7 @@ contract Lottery {
     function enroleInLottery(string memory playerName) public payable {
        require(msg.value == 1 ether);
        require(bytes(playerName).length > 0);
+       require(players.length < 10);
         
         player[msg.sender] = playerName;
         players.push(msg.sender);
@@ -54,15 +62,23 @@ contract Lottery {
         return players;
     }
 
-    function getAWinner() public returns(string memory){
-        require(players.length == 10, string(abi.encodePacked("We do not have 10 participants yet we have: ", players.length)));
+    function getNumberOfParticipants() public view returns(uint){
+        return players.length;
+    }
+
+    function getAWinner() public view returns(string memory){
+        
+        
+        require(players.length == 10, "We do not have 10 participants yet ");
         uint nbOfPlayers = players.length;
         uint randomHash = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
         uint winnerIndex = randomHash % nbOfPlayers;
 
-        address winner = players[winnerIndex];
+        address winner = players[winnerIndex];   
 
-        sendPrize(winner);
+       //sendPrize(winner);
+
+        //emit fml(player[winner], winner);
         
         return player[winner];
     }
@@ -70,7 +86,7 @@ contract Lottery {
     function getCalculatedPrizePool() public view returns (uint256){
         uint256 effectivePrize;        
         uint randomHash = uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
-        bool isJackpot = (randomHash % 100) < jackPotOdds;
+        bool isJackpot = (randomHash % 100) <= jackPotOdds;
 
         if(isJackpot){
             effectivePrize = prizePool + jackPot;
@@ -79,8 +95,8 @@ contract Lottery {
         return effectivePrize;
     }
     function sendPrize(address winner) private {
-      /*  uint prize = prizePool;
+        uint prize = prizePool;
         prizePool = 0;
-        payable(winner).transfer(prize);*/
+        payable(winner).transfer(prize);
     }
 }
--- test/lottery.js
+++ test/lottery.js
@@ -9,11 +9,11 @@ contract("Lottery", function ( accounts ) {
   
   let lottery;
 
-  before(async () =>{
-    lottery = await Lottery.deployed();
+  beforeEach(async () =>{
+    lottery = await Lottery.new();
   });
 
-  async function enroleTenPlayers(){
+  async function enroleTenPlayers(lottery){
     const wager = web3.utils.toWei('1', 'ether');
 
     let playerNames = ["player0", "player1", "player2", "player3", "player4", "player5", 
@@ -29,13 +29,12 @@ contract("Lottery", function ( accounts ) {
     return assert.isTrue(true);
   });
  
-  it("should allow multiple accounts to enroll", async () => {
-  //  const lottery = await Lottery.deployed();
+  it("should allow multiple accounts to enroll", async () => {   
     const wager = web3.utils.toWei('1', 'ether');
     const expectedPricePool = web3.utils.toWei('9', 'ether');
     const expectedJackPot = web3.utils.toWei('0.9', 'ether');
 
-    await enroleTenPlayers();
+    await enroleTenPlayers(lottery);
 
     const playersCount = await lottery.getParticipants();
     const pricePool = await lottery.getPricePool();
@@ -43,12 +42,10 @@ contract("Lottery", function ( accounts ) {
 
     assert.equal(playersCount.length, 10, "There should be exactly 10 players");
     assert.equal(expectedPricePool, pricePool, "The pricePool should be 9");
-    assert.equal(expectedJackPot, jackPot, "The jackpot should be 0.9");
-   
+    assert.equal(expectedJackPot, jackPot, "The jackpot should be 0.9");   
   });
 
-  it("should emit message when enrolling", async () => {
-   // const lottery = await Lottery.deployed();
+  it("should emit message when enrolling", async () => {  
     const wager = web3.utils.toWei('1', 'ether');
     const ginette = "Ginette Reno";
     const joel = "Joel Legendre";
@@ -59,55 +56,38 @@ contract("Lottery", function ( accounts ) {
     const resultGinette = tx1.logs[0].args.message; 
     const resultJoel = tx2.logs[0].args.message; 
     assert.isTrue(resultGinette.includes(ginette), "The event output should contain the player's name");  
-    assert.isTrue(resultJoel.includes(joel), "The event output should contain the player's name");  
-   
+    assert.isTrue(resultJoel.includes(joel), "The event output should contain the player's name");   
   });
 
-  it("should emit message when enrolling", async () => {
-    //const lottery = await Lottery.deployed();
-    const wager = web3.utils.toWei('1', 'ether');
-    const ginette = "Ginette Reno";
-    const joel = "Joel Legendre";
-    
-    const tx1 =  await lottery.enroleInLottery(ginette, {from: accounts[0], value: wager});      
-    const tx2 =  await lottery.enroleInLottery(joel, {from: accounts[0], value: wager});      
+  it("should have 10 players enrolled", async () => {   
+    let expectedPlayers = 10;   
 
-    const resultGinette = tx1.logs[0].args.message; 
-    const resultJoel = tx2.logs[0].args.message; 
-    assert.isTrue(resultGinette.includes(ginette), "The event output should contain the player's name");  
-    assert.isTrue(resultJoel.includes(joel), "The event output should contain the player's name");  
-   
+    await enroleTenPlayers(lottery);
+
+    let nbOfPlayers = await lottery.getNumberOfParticipants()    
+    
+    assert.equal(expectedPlayers, nbOfPlayers);     
   });
 
-  it("with jackpotOdds at 100%, should return prizePool + jackpot", async () => {
-    const lottery = await Lottery.deployed();
-    const expectedPrize = web3.utils.toWei('99', 'ether');
-    
-    const wager = web3.utils.toWei('1', 'ether');
-
-    let playerNames = ["player0", "player1", "player2", "player3", "player4", "player5", 
-                      "player6", "player7", "player8", "player9"];
-
-   /* for(let i = 0; i < 10; i++) {
-      await lottery.enroleInLottery(playerNames[i], {from: accounts[i], value: wager});  
-    }  */
-
-    console.log("Number of enroled Players: ");
-    console.log(lottery.getParticipants().length);
-
+  it("with jackpotOdds at 100%, should return prizePool + jackpot", async () => {    
+    const expectedPrize = web3.utils.toWei('9.9', 'ether');   
     await lottery.setJackPotOdds(100);
 
-    prize = await lottery.getCalculatedPrizePool();
-    await lottery.getAWinner();
-    
-    print(expectedPrize);
-    print(prize);
+    await enroleTenPlayers(lottery);    
 
-    assert.equal(expectedPrize, prize);
-   
+    prize = await lottery.getCalculatedPrizePool();    
+
+    assert.equal(expectedPrize, prize);   
   });
 
-  
+  it("picks a winner, and sends him money", async () => {    
+    let possibleWinners = ["player0", "player1", "player2", "player3", "player4", "player5", 
+                      "player6", "player7", "player8", "player9"];
+    await enroleTenPlayers(lottery);    
+       
+    let winner = await lottery.getAWinner();   
 
-//})
+    assert.isTrue (possibleWinners.includes(winner));
+  });
+
 });
